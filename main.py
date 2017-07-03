f2ea656a044636ea737e4083958356da12a187ca
Tri Tran <tritran20@gmail.com>
2017-07-03 14:18:28 -0700

    Added randomly generated function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,11 +9,16 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    nextmovie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
+    content += "</ul>"
+    content += "<h1>Tommorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + nextmovie + "</li>"
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
@@ -21,9 +27,12 @@ def index():
     return content
 
 def get_random_movie():
+
+    movie_list = ["The Hulk", "Captain American", "Iron Man", "Wonder Women", "X-Men"]
+    random_number = random.randrange(len(movie_list))
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    return movie_list[random_number]
 
 
 app.run()
